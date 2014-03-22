ee33b5f97dd52a8bed415ae21505a911276ed56a
Mramaizng <beersbr@gmail.com>
2014-03-22 01:11:08 -0700

    updated the index page to have some boilerplate and added some more base class code

--- scripts/game.js
+++ scripts/game.js
@@ -1,15 +1,63 @@
 GAME = (function(game){
-
 	// *************************************
 	// "classes"
 	// *************************************
+
+
+	// this function works off of naming conventions
+	// webglContext => a context object as webgl
+	// name => first part of the ids following -vertex and -fragment respectively
+	// uniforms => an array of uniform names as strings
+	// attribs => an array of attribs as strings
+	function ShaderProgram(webglContext, name, uniforms, attribs){
+		var _id = 0;
+
+		Object.setProperty(this, "id", {
+			set: function(){
+				console.warn("Cannot set readonly property id.");
+			},
+			get: function(){
+				return _id;
+			}
+		})
+
+		var vertexShaderName = name+"-vertex";
+		var fragmentShaderName = name+"-fragment";
+
+		this.vertexShader = createShaderFromScriptElement(webglContext, vertexShaderName);
+		this.fragmentShader = createShaderFromScriptElement(webglContext, fragmentShaderName);
+
+		this.binary = createProgram(webglContext, [this.vertexShader, this.fragmentShader]);
+
+		// go through the vars and attach them to the program
+
+
+		// make it so we can just reference the vars later off the ShaderProgram instance
+		// var shader = new ShaderProgram(ctx, "shaderIdPrefix", ["u_view_matrix"], ["a_position"])
+		// shader.a_position || shader.u_view_matrix || shader["a_position"] || shader["u_view_matrix"]
+		for(var uniIdx in uniforms){
+			this[uniforms[uniIdx]] = webglContext.getUniformLocation(this.binary, uniforms[uniIdx]);
+		}
+
+		for(var attrIdx in attribs){
+			this[attribs[attrIdx]] = webglContext.getAttribLocation(this.binary, [attribs[attrIdx]]);
+		}
+	}
 
 	function VertexData(texture, textureCoords){
 
 	}
 
+	function ImageEx(filepath){
+		var filename = filepath;
+
+		this.img = new Image();
+		this.img.src = filename;
+	}
+
 	function Entity(){
 		this.vertecies = new List.List();
+		this.shaderProgram = null;
 	}
 
 	function GameState(){
@@ -17,11 +65,17 @@ GAME = (function(game){
 	}
 
 	function Scene(){
+		this.objects = new List.List();
 
+		this.draw = function(){
+			this.objects.eachValue(function(n){
+
+			});
+		}
 	}
 
 	// *************************************
-	// functions
+	// Functions
 	// *************************************
 
 	var StartScreen_GameState = new GameState();
@@ -38,7 +92,12 @@ GAME = (function(game){
 		requestAnimationFrame(loop);
 	}
 
+	// *************************************
+	// Expose API
+	// *************************************
+
 	game.init = init;
+	game.loop = loop;
 
 	return game;
 })({});
